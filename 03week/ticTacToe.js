adaf651147d7c95d8a6fdf0ac4ec00ffe30757a3
William-Cruz <chiefxcruz@gmail.com>
2018-08-07 19:19:43 -0500

    practice run

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -64,22 +64,32 @@ if (typeof describe === 'function') {
   describe('#ticTacToe()', () => {
     it('should place mark on the board', () => {
       ticTacToe(1, 1);
-      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+      assert.deepEqual(board, [ [' ', ' ', ' '], 
+                                [' ', 'X', ' '], 
+                                [' ', ' ', ' '] ]);
     });
     it('should alternate between players', () => {
       ticTacToe(0, 0);
-      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+      assert.deepEqual(board, [ ['O', ' ', ' '], 
+                                [' ', 'X', ' '], 
+                                [' ', ' ', ' '] ]);
     });
     it('should check for vertical wins', () => {
-      board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
+      board = [ [' ', 'X', ' '], 
+                [' ', 'X', ' '], 
+                [' ', 'X', ' '] ];
       assert.equal(verticalWin(), true);
     });
     it('should check for horizontal wins', () => {
-      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
+      board = [ ['X', 'X', 'X'], 
+                [' ', ' ', ' '], 
+                [' ', ' ', ' '] ];
       assert.equal(horizontalWin(), true);
     });
     it('should check for diagonal wins', () => {
-      board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
+      board = [ ['X', ' ', ' '], 
+                [' ', 'X', ' '], 
+                [' ', ' ', 'X'] ];
       assert.equal(diagonalWin(), true);
     });
     it('should detect a win', () => {
